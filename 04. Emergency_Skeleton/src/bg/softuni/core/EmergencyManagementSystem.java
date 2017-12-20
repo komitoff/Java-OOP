13c6cac25044b8de78fe7019b322472faacc165d
komitoff <todorkomitov@gmail.com>
2017-12-20 18:03:35 +0200

    Initial commit emergency task structure

---
+++ 04. Emergency_Skeleton/src/bg/softuni/core/EmergencyManagementSystem.java
@@ -0,0 +1,40 @@
+package bg.softuni.core;
+
+public class EmergencyManagementSystem {
+
+    public EmergencyManagementSystem() {
+
+    }
+
+    public String registerPropertyEmergency() {
+        return null;
+    }
+
+    public String registerHealthEmergency() {
+        return null;
+    }
+
+    public String registerOrderEmergency() {
+        return null;
+    }
+
+    public String registerFireServiceCenter() {
+        return null;
+    }
+
+    public String registerMedicalServiceCenter() {
+        return null;
+    }
+
+    public String registerPoliceServiceCenter() {
+        return null;
+    }
+
+    public String processEmergencies() {
+        return null;
+    }
+
+    public String emergencyReport() {
+        return null;
+    }
+}
